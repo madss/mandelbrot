b139d9c28a5d955f9816d2b88a22c3accac979bd
Mads Sejersen <mads@madss.net>
2016-02-09 23:07:32 +0100

    Bunch of improvements

--- mandelbrot.js
+++ mandelbrot.js
@@ -2,9 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var MandelbrotCanvas = function(canvas, width, height) {
     this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.resize(width, height);
+  };
+
+  MandelbrotCanvas.prototype.resize = function(width, height) {
     this.width = width;
     this.height = height;
-    this.ctx = this.canvas.getContext('2d');
     this.canvas.width = this.width;
     this.canvas.height = this.height;
     this.imageData = this.ctx.getImageData(0, 0, this.width, this.height);
@@ -24,45 +28,50 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   var Mandelbrot = function(width, height, max) {
-    this.width = width;
-    this.height = height;
+    this.canvasWidth = width;
+    this.canvasHeight = height;
     this.max = max;
-    this.reCenter = -0.5;
-    this.imCenter = 0.0;
+
     this.scale = 1;
-    // FIXME: Fit inside re [-2, 1], im [-1, 1] while keeping aspect ratio
-    this.reSize = 3;
-    this.imSize = 2;
+    this.centerX = -0.75;
+    this.centerY = 0.0;
+    this.initialWidth = 2.6;
+    this.initialHeight = 2.4;
+
+    var canvasRatio = this.canvasWidth/this.canvasHeight;
+    var ratio = this.initialWidth/this.initialHeight;
+    if (canvasRatio < ratio) {
+      this.initialHeight += this.initialHeight*(1/canvasRatio - 1/ratio);
+    } else {
+      this.initialWidth += this.initialWidth*(canvasRatio - ratio);
+    }
+    this.width = this.initialWidth;
+    this.height = this.initialHeight;
   };
 
-  Mandelbrot.prototype.zoom = function(x, y, factor) {
-    var reSize = this.reSize*this.scale;
-    var imSize = this.imSize*this.scale;
-    this.reCenter = this.reCenter - reSize/2 + (x/this.width)*reSize;
-    this.imCenter = this.imCenter - imSize/2 + (y/this.height)*imSize;
-    this.scale = this.scale/factor;
+  Mandelbrot.prototype.zoom = function(canvasX, canvasY, factor) {
+    this.centerX += -0.5*this.width + (canvasX/this.canvasWidth)*this.width;
+    this.centerY += -0.5*this.height + (canvasY/this.canvasHeight)*this.height;
+    this.scale /= factor;
+    this.width = this.initialWidth*this.scale;
+    this.height = this.initialHeight*this.scale;
   };
 
-  Mandelbrot.prototype.escape = function(x, y) {
-    var re, im, q, i, x, y, xtmp, ytmp, logZn, nu;
-
-    // FIXME: Doesn't belong here
-    // Calculate the model size of the new area to draw
-    var reSize = this.reSize*this.scale;
-    var imSize = this.imSize*this.scale;
+  Mandelbrot.prototype.escape = function(canvasX, canvasY) {
+    var x0, y0, q, i, x, y, xtmp, ytmp, logZn, nu;
 
     // Find the point of interest
-    re = this.reCenter - reSize/2 + (x/this.width)*reSize;
-    im = this.imCenter - imSize/2 + (y/this.height)*imSize;
+    x0 = this.centerX - 0.5*this.width + (canvasX/this.canvasWidth)*this.width;
+    y0 = this.centerY - 0.5*this.height + (canvasY/this.canvasHeight)*this.height;
 
     // Skip if inside the cardioid
-    q = (re - 0.25)*(re - 0.25) + im*im;
-    if ((q*(q + (re - 0.25))) < 0.25*im*im) {
+    q = (x0 - 0.25)*(x0 - 0.25) + y0*y0;
+    if ((q*(q + (x0 - 0.25))) < 0.25*y0*y0) {
       return this.max;
     }
 
     // Skip if inside the period-2 bulb
-    if ((re + 1)*(re + 1) + im*im < 1/16) {
+    if ((x0 + 1)*(x0 + 1) + y0*y0 < 1/16) {
       return this.max;
     }
 
@@ -70,12 +79,14 @@ document.addEventListener('DOMContentLoaded', function() {
     i = 0;
     x = y = 0;
     while (x*x + y*y <= 256 && i < this.max) {
-      xtmp = x*x - y*y + re;
-      ytmp = 2*x*y + im;
+      xtmp = x*x - y*y + x0;
+      ytmp = 2*x*y + y0;
+
       // Abort if nothing changes
       if (x === xtmp && y === ytmp) {
         return this.max;
       }
+
       x = xtmp;
       y = ytmp;
       i++;
@@ -92,10 +103,10 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
   Mandelbrot.prototype.render = function(draw) {
-    var x, y;
-    for (y = 0; y < this.height; y++) {
-      for (x = 0; x < this.width; x++) {
-        draw(x, y, this.escape(x, y));
+    var canvasX, canvasY;
+    for (canvasY = 0; canvasY < this.canvasHeight; canvasY++) {
+      for (canvasX = 0; canvasX < this.canvasWidth; canvasX++) {
+        draw(canvasX, canvasY, this.escape(canvasX, canvasY));
       }
     }
   };
@@ -108,56 +119,59 @@ document.addEventListener('DOMContentLoaded', function() {
     return end - start;
   }
 
-  var GradientPalette = function(max, from, to) {
-    this.max = max;
-    this.from = from;
-    this.to = to;
+  var randomPalette = function(max) {
+    var p, n, i;
+    p = [];
+    n = Math.floor(max*Math.random());
+    for (i = 0; i < n; i++) {
+      p.push({
+        r: Math.floor(255*Math.random()),
+        g: Math.floor(255*Math.random()),
+        b: Math.floor(255*Math.random())
+      });
+    }
+    return p;
   };
-
-  GradientPalette.prototype.value = function(i, c) {
-    return Math.floor(255*((i/this.max)*(this.to[c] - this.from[c]) + this.from[c]));
-  }
-
-  GradientPalette.prototype.r = function(i) { return this.value(i, 'r'); };
-  GradientPalette.prototype.g = function(i) { return this.value(i, 'g'); };
-  GradientPalette.prototype.b = function(i) { return this.value(i, 'b'); };
 
   var max = 500;
   var canvas = document.querySelector('canvas');
-  var mandelbrotCanvas = new MandelbrotCanvas(canvas, window.innerWidth, window.innerHeight);
-  var mandelbrot = new Mandelbrot(mandelbrotCanvas.width, mandelbrotCanvas.height, max);
-  var palette = [
-    { r: 255, g:   0, b:   0 },
-    { r:   0, g: 255, b:   0 },
-    { r:   0, g:   0, b: 255 },
-  ];
+  var mandelbrotCanvas = new MandelbrotCanvas(
+    canvas,
+    window.innerWidth,
+    window.innerHeight
+  );
+  var mandelbrot = new Mandelbrot(
+    mandelbrotCanvas.width,
+    mandelbrotCanvas.height,
+    max
+  );
+  var palette = randomPalette(max);
   var render = function() {
-    mandelbrot.render(function(x, y, i) {
+    mandelbrot.render(function(canvasX, canvasY, i) {
+      var r = 0, g = 0, b = 0;
       if (i < max) {
         var n = Math.floor(i);
         var f = i%1;
         var color1 = palette[n%palette.length];
         var color2 = palette[(n + 1)%palette.length];
-        var r = Math.floor(color1.r - f*(color1.r - color2.r));
-        var g = Math.floor(color1.g - f*(color1.g - color2.g));
-        var b = Math.floor(color1.b - f*(color1.b - color2.b));
-        mandelbrotCanvas.putPixelRGB(x, y, r, g, b);
-      } else {
-        mandelbrotCanvas.putPixelRGB(x, y, 0, 0, 0);
+        r = Math.floor(color1.r - f*(color1.r - color2.r));
+        g = Math.floor(color1.g - f*(color1.g - color2.g));
+        b = Math.floor(color1.b - f*(color1.b - color2.b));
       }
+      mandelbrotCanvas.putPixelRGB(canvasX, canvasY, r, g, b);
     });
     mandelbrotCanvas.draw();
   };
 
   canvas.addEventListener('click', function(e) {
-    var x = e.pageX - canvas.offsetLeft;
-    var y = e.pageY - canvas.offsetTop;
+    var canvasX = e.pageX - canvas.offsetLeft;
+    var canvasY = e.pageY - canvas.offsetTop;
     var zoom = e.shiftKey ? 1.1 : 5.0;
     if (e.ctrlKey) {
       zoom = 1.0/zoom;
     }
-    mandelbrot.zoom(x, y, zoom);
-    console.log('Location:', mandelbrot.reCenter, mandelbrot.imCenter, mandelbrot.scale);
+    mandelbrot.zoom(canvasX, canvasY, zoom);
+    console.log('Location:', mandelbrot.centerX, mandelbrot.centerY, mandelbrot.scale);
     render();
   });
 
